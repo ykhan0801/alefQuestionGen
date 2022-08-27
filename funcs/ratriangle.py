168f594f0c2e3ac053ec2426fdfb0fca41f798d2
yousufkhan <yousufkhan6760@gmail.com>
2022-08-27 10:09:00 +0100

    V3

--- funcs/ratriangle.py
+++ funcs/ratriangle.py
@@ -10,8 +10,10 @@ def ratriangle(quantity):
         randBase = random.randint(5, 10)
         randHeight = random.randint(5, 10)
 
-        baseCoord = str((randBase*10) + 50)
-        height = str((randHeight*10) + 50)
+        base = str((randBase*25) + 50)
+        base = '"{}"'.format(base)
+        height = str(400 - ((randHeight*25) + 50))
+        height = '"{}"'.format(height)
 
         answer = round(math.sqrt((randBase*randBase)+(randHeight*randHeight)), 2)
 
@@ -20,12 +22,12 @@ def ratriangle(quantity):
 
         svgList.append("""
         <svg xmlns="http://www.w3.org/2000/svg" width="400" height="400">
-        <line x1="50" y1="350" x2="350" y2="350" stroke="white" stroke-width="3"/>
-        <line x1="50" y1="350" x2="50" y2="50" stroke="white" stroke-width="3"/>
-        <line x1="50" y1="50" x2="350" y2="350" stroke="white" stroke-width="3"/>
-        <text x="160" y = "340" class="base" stroke="white" fill="white" font-family="Arial"> """ + str(randBase) + """"cm </text>
-        <text x="60" y = "200" class="height" stroke="white" fill="white" font-family="Arial"> """ + str(randHeight) + """"cm </text>
-        <text x="190" y = "170" class="base" stroke="red" fill="red" font-family="Arial"> ? </text>
+        <line x1="50" y1=""" + height + """ x2=""" + base + """ y2="350" stroke="white" stroke-width="3"/>
+        <line x1="50" y1="350" x2="50" y2=""" + height + """ stroke="white" stroke-width="3"/>
+        <line x1="50" y1="350" x2=""" + base + """ y2="350" stroke="white" stroke-width="3"/>
+        <text x="90" y = "370" class="base" stroke="white" fill="white" font-family="Arial"> """ + str(randBase) + """"cm </text>
+        <text x="5" y ="260" class="height" stroke="white" fill="white" font-family="Arial"> """ + str(randHeight) + """"cm </text>
+        <text x="200" y = "200" class="base" stroke="red" fill="red" font-family="Arial"> ? </text>
         </svg>""")
 
         i += 1
